7891a1b9ec4a24e6f4783d4cdc4333f064d0a6b4
Sjjjiayou <18016457253@163.com>
2023-06-24 17:51:45 +0800

    feat: 实现一个自定义loader

--- src/view/app.tsx
+++ src/view/app.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import './index.css'; // 导入css
-import InfoIcon20 from '@/assets/icons/info-20.svg';
+// import InfoIcon20 from '@/assets/icons/info-20.svg';
 
 const App = () => {
-  return (
-    <div>
-      你
-      <InfoIcon20 />
-    </div>
-  );
+  return <div>我要学好前端，因为学好前端可以: {/* <InfoIcon20 /> */}</div>;
 };
 
 export default App;
